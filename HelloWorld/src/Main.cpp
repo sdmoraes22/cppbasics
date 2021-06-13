26c8000de642cda5039b3cab4a9b735e853b5ac5
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-06-13 17:45:03 -0400

    Build script

--- HelloWorld/src/Main.cpp
+++ HelloWorld/src/Main.cpp
@@ -1,9 +1,20 @@
-#include "hello.hpp"
 #include <iostream>
 
 int main()
 {
-    hello::say_hello();
+    unsigned int variable = 8; //2b -> -2b
+    char a = 'A';
+    float variablef = 5.5f;
+    double variabled = 5.5;
+    bool boolean = true;
+
+    std::cout << variable << std::endl;
+    variable = 20;
+    std::cout << variable << std::endl;
+    std::cout << a << std::endl;
+    std::cout << variablef << std::endl;
+    std::cout << variabled << std::endl;
+    std::cout << boolean << std::endl;
+    std::cout << sizeof(bool) << std::endl;
     std::cin.get();
 }
-
