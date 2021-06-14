5948dc7f0510eb2a067d761070c0cfe3d6fc2db3
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-06-14 17:46:22 -0400

    Functions Tutorial

---
+++ Functions/build/CMakeFiles/Functions.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Functions.dir/src/Main.cpp.o"
+  "CMakeFiles/Functions.dir/src/Main.cpp.o.d"
+  "Functions"
+  "Functions.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Functions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Functions/src/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int Multiply(int a, int b)
+{
+    return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+    int result = Multiply(a, b);
+    std::cout << result << std::endl;
+}
+
+int main()
+{
+    MultiplyAndLog(3, 2);
+    MultiplyAndLog(5, 8);
+    MultiplyAndLog(90, 45);
+
+    std::cin.get();
+
+}
+
