622b28bf123344932a49e1f9e19c65db8921fa72
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-06-15 20:57:21 -0300

    Class basic usage

---
+++ ClassUsageExample/build/CMakeFiles/ClasseUsageExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClasseUsageExample.dir/src/ClassUsageExample.cpp.o"
+  "CMakeFiles/ClasseUsageExample.dir/src/ClassUsageExample.cpp.o.d"
+  "ClasseUsageExample"
+  "ClasseUsageExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClasseUsageExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ClassUsageExample/build/CMakeFiles/ClasseUsageExample.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ClasseUsageExample.
+# This may be replaced when dependencies are built.
