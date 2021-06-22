5ef1cccf02cb1bb30d8d15c6796e3417a76d92a1
Cristiano Moraes <cmoraes.ti@gmail.com>
2021-06-21 21:21:17 -0300

    Enums in c++

---
+++ Enums/src/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+enum Example : unsigned short
+{
+    A = 1, B = 2, C = 3
+};
+
+int main()
+{
+    Example value = B;
+
+    if(value == 2)
+    {
+
+    }
+    std::cin.get();
+}
